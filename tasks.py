8d5e83dc7c246f40ba82de9068f748a8ae1a1512
Max Kharandziuk <max.kharandziuk@gmail.com>
2021-01-22 12:45:38 +0100

    add pre-commit-hook

--- tasks.py
+++ tasks.py
@@ -1,17 +1,28 @@
 from invoke import task
-from pathlib import Path 
+from pathlib import Path
 
-ROOT_DIR =Path()
+ROOT_DIR = Path()
+
+
+@task
+def format(c):
+    c.run("pre-commit install")
+    c.run("pre-commit run --all-files")
+
 
 @task
 def orchestration(c):
-    c.run('vagrant up')
+    c.run("vagrant up")
+
 
 @task
 def provision(c):
-    SSH_KEY = ROOT_DIR / '.vagrant' / 'machines' / 'server' / 'virtualbox' / 'private_key'
-
-    c.run(f"ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i 10.2.2.25,  --user=vagrant --key-file {SSH_KEY} playbook.yml")
+    SSH_KEY = (
+        ROOT_DIR / ".vagrant" / "machines" / "server" / "virtualbox" / "private_key"
+    )
+    c.run(
+        f"ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i 10.2.2.25,  --user=vagrant --key-file {SSH_KEY} playbook.yml"
+    )
 
 
 @task
