cb5b9904a246e89ba15a3d81bf1442cf4bf23f4a
Max Kharandziuk <max.kharandziuk@gmail.com>
2021-01-22 12:38:45 +0100

    add simple role

--- tasks.py
+++ tasks.py
@@ -1,9 +1,20 @@
 from invoke import task
+from pathlib import Path 
+
+ROOT_DIR =Path()
 
 @task
 def orchestration(c):
     c.run('vagrant up')
 
-@task(orchestration)
+@task
 def provision(c):
-    c.run(f"ansible-playbook -i 10.2.2.25, playbook.yml")
+    SSH_KEY = ROOT_DIR / '.vagrant' / 'machines' / 'server' / 'virtualbox' / 'private_key'
+
+    c.run(f"ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i 10.2.2.25,  --user=vagrant --key-file {SSH_KEY} playbook.yml")
+
+
+@task
+def deploy(c):
+    orchestration(c)
+    provision(c)
